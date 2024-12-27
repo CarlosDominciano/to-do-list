cc90061b06a6ac6c5b9fafd7250d69d4ea1e07d5
Carlos Dominciano <carlos.dominciano@gmail.com>
2024-12-27 10:18:38 -0300

    Back-End: Docker file created (DONE)

--- App/todo/src/main/java/com/app/todo/config/CorsConfig.java
+++ App/todo/src/main/java/com/app/todo/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.app.todo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permite todas as rotas
-                .allowedOrigins("http://localhost:3000") // Origem permitida (React, por exemplo)
+                .allowedOrigins(frontendUrl) // Origem permitida (React, por exemplo)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                 .allowedHeaders("*") // Permite todos os cabeçalhos
                 .allowCredentials(true); // Permite envio de credenciais (cookies, auth, etc.)
